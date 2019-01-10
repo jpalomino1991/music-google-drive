d5692cccec1b8df9599257d60d75a23c9b8d8b8d
juancjara <juanc.jara@pucp.pe>
2019-01-10 16:14:25 -0500

    husky test 2

--- backend/src/auth.js
+++ backend/src/auth.js
@@ -4,13 +4,13 @@ const { google } = require('googleapis');
 const db = require('./db');
 
 const oauth2Client = new google.auth.OAuth2(
-process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URL
 );
 
 const scopes = [
-  "https://www.googleapis.com/auth/userinfo.profile",
+  'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/drive.readonly'
 ];
 
