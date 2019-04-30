7c6eff9a28d79aa8144bb2d0d5addc4bbd5b094c
juancjara <juancjara@pucp.pe>
2019-04-30 12:55:39 -0500

    heroku deploy frontend

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Login = () => {
   return (
-    <a href={`${process.env.REACT_APP_BACKEND_URL}/google/login`}>login</a>
+    <a href={`${process.env.REACT_APP_BACKEND_URL}/google/login`}>
+      {JSON.stringify(process.env)}login
+    </a>
   );
 };
 
