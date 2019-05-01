c8f0d9ac958dd95bac1812a5a182d4be00e645bf
juancjara <juancjara@pucp.pe>
2019-04-30 19:51:50 -0500

    frontend: remove debug

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const Login = () => {
   return (
-    <a href={`${process.env.REACT_APP_BACKEND_URL}/google/login`}>
-      {JSON.stringify(process.env)} login
-    </a>
+    <a href={`${process.env.REACT_APP_BACKEND_URL}/google/login`}>login</a>
   );
 };
 
