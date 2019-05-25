3bbc30c3a123a75ed7acb9047ad229053be6b0e1
juancjara <juancjara@pucp.pe>
2019-05-25 14:58:30 -0500

    Remove test code to skip folder query validation

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -23,7 +23,6 @@ const Home = props => {
   return (
     <Query query={FETCH_FOLDERS}>
       {({ loading, data }) => {
-        return <FolderPicker />;
         if (loading) return <div>loading</div>;
         if (!data.folders.length) return <FolderPicker />;
         const folder = data.folders[0];
