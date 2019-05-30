be8f2211525deecf737f3f79dc1398dc603208be
juancjara <juancjara@pucp.pe>
2019-05-30 14:25:53 -0500

    Add css prop from styled-components

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,9 +1,13 @@
-const CracoWorkboxPlugin = require("craco-workbox");
+const CracoWorkboxPlugin = require('craco-workbox');
+//const BabelRcPlugin = require('@jackwilsdon/craco-use-babelrc');
 
 module.exports = {
+  babel: {
+    plugins: ['babel-plugin-styled-components'],
+  },
   plugins: [
     {
-      plugin: CracoWorkboxPlugin
-    }
-  ]
+      plugin: CracoWorkboxPlugin,
+    },
+  ],
 };
